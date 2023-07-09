034f0612e18265bbf3fde00ddfa1a74cf4c9e43e
Ammar <bhatmmr@gmail.com>
2023-07-09 14:34:17 +0530

    fixing errors

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import { Link } from "react-scroll"
-import { motion } from "framer-motion"
 function Card(props) {
   return (
    
-        <motion.div className='w-96 flex h-64 border-2 sm:w-1/3 border-gray-400 rounded-lg m-7 sm:m-10 hover:shadow-xl shadow-xl shadow-gray-400 hover:shadow-gray-500'
-          initial={{opacity: 0.2}}
-          whileInView={{opacity: 1}}
-          viewport={{once: true, amount: 0.7}}
-        >
+        <div className='w-96 flex h-64 border-2 sm:w-1/3 border-gray-400 rounded-lg m-7 sm:m-10 hover:shadow-xl shadow-xl shadow-gray-400 hover:shadow-gray-500'>
         <img src={props.img} className="w-32 md:w-32 sm:w-40 rounded-lg border-r-2 border-gray-400 object-cover" />
         <div className='text-center'> 
         <h2 className='mt-2 mb-2 text-2xl font-medium text-gray-800'>{props.name}</h2>
@@ -21,7 +16,7 @@ function Card(props) {
         <Link to="Contact" smooth={true} duration={500}><button className='sm:p-1 p-2 px-5 rounded-full mb-0 font-medium bg-green-400'>ORDER</button></Link> 
         </div>
   
-    </motion.div>
+    </div>
   )
 }
 
